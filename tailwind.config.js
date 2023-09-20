bad30435da99b9bf7a57ee1bc72627676f882ef6
fatihbatu <77248966+fatihbatu@users.noreply.github.com>
2023-09-20 16:49:20 +0300

    initialize

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layouts/*.liquid',
+    './sections/*.liquid',
+    './templates/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
